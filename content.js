c63ecf30da4f9b8881ce808fdbc9518a6df60e4c
tunnckoCore <mameto2011@gmail.com>
2017-10-02 22:49:39 +0300

    upload to github
    Signed-off-by: tunnckoCore <mameto2011@gmail.com>

---
+++ content.js
@@ -0,0 +1,18 @@
+/* global HideFilesOnGitHub */
+
+'use strict';
+
+const domLoaded = new Promise(resolve => {
+	if (document.readyState === 'loading') {
+		document.addEventListener('DOMContentLoaded', resolve);
+	} else {
+		resolve();
+	}
+});
+
+domLoaded.then(function init () {
+	let emojis = document.querySelectorAll('table a[title] g-emoji[alias]')
+	for (let emoj of emojis) {
+		emoj.setAttribute('title', emoj.parentNode.getAttribute('title'))
+	}
+});
